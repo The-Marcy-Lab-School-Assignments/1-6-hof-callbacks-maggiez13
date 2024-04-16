88065d99c99e2322958a61b513964c65fe34fcbb
Mei Zhu <maggiez0613@gmail.com>
2024-04-16 11:44:14 -0400

    q3 done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -15,7 +15,18 @@ const myMap = (arr, callback) => {
   return newArr;
 };
 
-const myFind = () => {
+const myFind = (arr, callback) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      // if callback of value is true, return value 
+      return arr[i];
+    }
+  }
+  return undefined;
+  // outside for loop since if callback never returns true
+  // loops through whole array first 
+    // don't need as an else since if statement answers question already ? 
+      // didn't work with else statement 
 };
 
 const myFilter = () => {
