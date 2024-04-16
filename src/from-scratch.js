59b29b84bafe415db8fff83d8e7a841c9102ebab
Mei Zhu <maggiez0613@gmail.com>
2024-04-16 11:27:53 -0400

    q2 done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,7 +1,18 @@
-const myForEach = () => {
+const myForEach = (arr, callback) => {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i]);
+    // passes each element of the array to the callback function
+  }
 };
 
-const myMap = () => {
+const myMap = (arr, callback) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(callback(arr[i]));
+    // let element = callback(arr[i]);
+    // newArr.push(element);
+  }
+  return newArr;
 };
 
 const myFind = () => {
